0f2cd26dd6f6f028c812bf6cf2cf9504ce8ec6fc
Arthur Jenck <arthurjenckdev@gmail.com>
2025-12-01 15:55:07 +0100

    feat: added example shuffling and accuracy check

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from perceptron import Perceptron
 
-perceptron = Perceptron()
+n_iterations = 2000
+
+perceptron = Perceptron(n_iterations=n_iterations)
 
 X, y = perceptron.getData('iris.csv')
 
 perceptron.fit(X, y)
 
-# example = X[0]
-# prediction_class = perceptron.predict(example, perceptron.weights)
+for i in range(10):
+    example = X[i]
+    prediction = perceptron.predict_class(example)
+
+accuracy = perceptron.score(X, y)
+print(f"Precision: {accuracy * 100:.2f}%")
--- perceptron.py
+++ perceptron.py
@@ -2,9 +2,11 @@ import csv
 import numpy as np
 
 class Perceptron:
-    def __init__(self, learning_rate=0.1, n_iterations=5):
+    def __init__(self, learning_rate=0.01, n_iterations=5):
         self.learning_rate = learning_rate
         self.n_iterations = n_iterations
+        self.label_map = {"setosa": 0, "versicolor": 1, "virginica": 2}
+
 
     def getData(self, filename):
         X_data = []
@@ -40,8 +42,7 @@ class Perceptron:
     def fit(self, X, y):
         learning_rate = self.learning_rate
         n_iterations = self.n_iterations
-
-        label_map = {"setosa": 0, "versicolor": 1, "virginica": 2}
+        label_map = self.label_map
 
         y_encoded=[]
         for label in y:
@@ -54,10 +55,14 @@ class Perceptron:
         self.weights = np.random.randn(n_classes, 4)
 
         for epoch in range(n_iterations):
+            indices = np.random.permutation(len(X))
+            X_shuffled = X[indices]
+            y_shuffled = y_encoded[indices]
+            
             for neuron in range(n_classes):
-                for i in range(len(X)):
-                    example = X[i]
-                    true_class = y_encoded[i]
+                for i in range(len(X_shuffled)):
+                    example = X_shuffled[i]
+                    true_class = y_shuffled[i]
 
                     true_response = 1 if true_class == neuron else 0
 
@@ -67,4 +72,23 @@ class Perceptron:
 
                     self.weights[neuron] += learning_rate * error * example
 
-   
+    def predict_class(self, example):
+        scores = []
+        for neuron in range(3):
+            score = self.scalar_product(example, self.weights[neuron])
+            scores.append(score)
+
+        return np.argmax(scores)
+
+    def score(self, X, y):
+        label_map = self.label_map
+        y_encoded = np.array([label_map[label] for label in y])
+
+        correct = 0
+        for i in range(len(X)):
+            prediction = self.predict_class(X[i])
+            if prediction == y_encoded[i]:
+                correct += 1
+
+        accuracy = correct / len(X)
+        return accuracy
